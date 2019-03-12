6043ebf1f0d492eee7607ca1b1464119b88e9e85
Theodore Wu <twu00@stuy.edu>
2019-03-12 11:03:54 -0400

    made dutch partition

--- Quick.java
+++ Quick.java
@@ -32,7 +32,7 @@ public class Quick{
       if (k < end){start = prevStart; end--;}
       if (k > end){end = prevEnd; start++;}
       //printArray(data);
-    } 
+    }
     return data[pivPos];
   }
   public static void fillRandom(int[] data){
@@ -41,6 +41,16 @@ public class Quick{
     for(int i = 0; i < data.length; i++){
       data[i] = i;
     }
+    for (int i = 0; i < data.length; i++){
+      int copy = data[i];
+      int rand = r.nextInt(data.length);
+      data[i] = data[rand];
+      data[rand] = copy;
+    }
+  }
+  public static void scramble(int[] data){
+    Random r = new Random();
+    int index = 0;
     for (int i = 0; i < data.length; i++){
       int copy = data[i];
       int rand = r.nextInt(data.length);
@@ -58,6 +68,45 @@ public class Quick{
     }
     System.out.println(out+"]");
   }
+  public static int[] partitionDutch(int[] data,int lo, int hi){
+    if (hi == lo) {return new int[]{lo,lo};}
+    int[] out = new int[2];
+    Random r = new Random();
+    int pick = r.nextInt(hi - lo) + lo;
+
+    int copy = data[lo];
+    data[lo] = data[pick]; //swap with lowest
+    data[pick] = copy;
+
+    int lt = lo+1;int gt = hi; int v = data[lo];
+    int i = lt;
+    while (i != gt){
+      if (data[i] == v){
+        i++;
+      }
+      else if (data[i] < v){
+        copy = data[i];
+        data[i] = data[lt];
+        data[lt] = copy;
+        lt++;
+        i++;
+      }
+      else if (data[i] > v){
+        copy = data[i];
+        data[i] = data[gt];
+        data[gt] = copy;
+        gt--;
+      }
+      printArray(data);
+    }
+
+    out[0] = lt; out[1] = gt; printArray(out);
+    return out;
+  }
+  public void quicksort(int[] data){
+
+  }
+
   public static void main(String[] args){
     int[] data = new int[10];
     /*fillRandom(data);
@@ -68,7 +117,8 @@ public class Quick{
     int select = (int) Math.abs(Math.random() * data.length);
     System.out.println(select+":"+quickselect(data, select));
    */
-    data = new int[] {1,2,4,4,4,4,4,4,4,2,3,3,3,2,2,4,5,6,4,5,6,8,7,5,3,2,4,6,7,8,9,7,5,4,3,2,2,2,4,4,5,6,7};
-    System.out.println("hi:"+quickselect(data,20));
+    data = new int[] {1,2,3,3,3,3,3,4,5,6};
+    scramble(data);
+    partitionDutch(data,0,data.length - 1);
   }
 }
